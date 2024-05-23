98cdda125ec42adcf31097bae0f152c6e26bf06b
Xiaohan Liu <xhliu127@yahoo.com>
2024-05-23 03:15:18 -0500

    completed hwk2

--- python_basics/class02/exercise_6.py
+++ python_basics/class02/exercise_6.py
@@ -23,8 +23,11 @@ def calculation(a, b):
 	'''
 
 	# code up your solution here
-
-
+	if isinstance(a, (int, float, complex)) and isinstance(b, (
+	int, float, complex)):
+		return (a + b, a - b)
+	else:
+		return 'Gee, see the function name? Feed me numbers!'
 
 def triangle_lambda():
 	'''
@@ -37,7 +40,7 @@ def triangle_lambda():
 	Returns:
 	lambda_triangle_area: the lambda
 	'''
-
+	return lambda base, height: base * height / 2
 
 
 def sort_words(hyphen_str):
@@ -59,10 +62,11 @@ def sort_words(hyphen_str):
 	'''
 
 	# code up your solution here
+	srt_words = hyphen_str.split('-').sort()
+	return '-'.join(srt_words)
 
 
-
-def perfect_number():
+def perfect_number(number):
 	'''
 	Write a Python function to check whether a number is perfect or not.
 
@@ -82,6 +86,20 @@ def perfect_number():
 	'''
 
 	# code up your answer here
+	if not isinstance(number, int) and number > 0:
+		# if not an N+, false
+		return False
+	else:
+	# Go thru all the interger j from 1 to sqrt(number), if j is a divisor of 
+	# number, then number/j is also a divisor of number, add them both to the
+	# sum of divisors. Start from 2 cause 1 is an obvious divisor.
+		sum_div = 1
+		for j in range(2, int(number**0.5)+1):
+			if number % j == 0:
+				sum_div += j + number // j
+		# If the sum of divisors equals the number, then the number is perfect
+		return sum_div == number
+
 
 
 if __name__ == '__main__':
